29ffd5ce5d166b98f2424099d145f16379994fb8
zhitongLIU <zhitonggm.liu@gmail.com>
2018-07-16 22:02:06 +0200

    Add respon core module

---
+++ lib/respon_core.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'respon_core/version'
+
+module ResponCore
+  # Your code goes here...
+end
+
+Dir['./concepts/*.rb'].each { |f| require f }
+Dir['./concepts/**/*.rb'].each { |f| require f }
+Dir['./lib/**/*.rb'].each { |f| require f }
+Dir['./formatters/**/*.rb'].each { |f| require f }
---
+++ spec/respon_core_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ResponCore do
+  it 'has a version number' do
+    expect(ResponCore::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
