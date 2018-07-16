d5022f0e33bca190de2bc094793f3ad1caaeeeba
zhitongLIU <zhitonggm.liu@gmail.com>
2018-07-16 22:01:41 +0200

    Add github repositroy manager services

---
+++ lib/rms/github.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module ResponCore
+  # repository management services
+  module Rms
+    # github module
+    class Github
+      attr_accessor :connection
+
+      def initialize
+        # Octokit.configure do |c|
+        #   c.auto_paginate = true
+        # end
+
+        self.connection ||= Octokit::Client.new(
+          access_token: ENV['GITHUB_ACCESS_TOKEN']
+        )
+      end
+    end
+  end
+end
